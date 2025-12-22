13355ea612d16b8b58308d21c420e8c1043fe4f7
Yugandhar <nanda.yugandhar@gmail.com>
2025-12-22 13:32:01 +0530

    Improved the performance of push little

--- benchmark_vectors.cpp
+++ benchmark_vectors.cpp
@@ -405,7 +405,7 @@ static void BM_VectorIteration(benchmark::State &state)
 
 #define ITERATIONS 30           // Statistically significant (n>=30 for CLT)
 #define START_SIZE int(10)
-#define END_SIZE int(1e8)
+#define END_SIZE int(1e6)
 #define RANGE_MULTIPLIER 10
 
 // Register the function as a benchmark
